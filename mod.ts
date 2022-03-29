4c1f604df7c84374941015ce55e554597b41851e
WhyK <windchimeyk@gmail.com>
2022-03-30 00:11:12 +0900

    refactor: add JSDoc

--- mod.ts
+++ mod.ts
@@ -1,10 +1,12 @@
 import type { CSS } from "./deps.ts";
 
+/**
+ * 文字列内のUppercaseを判別し、Lowercaseに変換する
+ */
 const removeUppercase = (prop: string) => {
   const uppercaseRegex = /([A-Z])/g;
   if (uppercaseRegex.test(prop)) {
-    return prop.replace(uppercaseRegex, (match) =>
-      `-${match.toLowerCase()}`);
+    return prop.replace(uppercaseRegex, (match) => `-${match.toLowerCase()}`);
   } else return prop;
 };
 
@@ -13,6 +15,9 @@ const removeUppercase = (prop: string) => {
  */
 export const properties = (style: CSS.Properties): CSS.Properties => style;
 
+/**
+ * CSSルールのObjectをstyle属性に利用できる文字列に変換
+ */
 export const convertStyle = (style: CSS.Properties): string => {
   const obj = Object(style);
   let styleString = "";
